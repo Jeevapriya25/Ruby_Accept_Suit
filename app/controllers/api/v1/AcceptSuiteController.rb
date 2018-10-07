6f6662faa068d3745ebc2394d313de7bd8788218
JeevapriyaC <30900716+JeevapriyaC@users.noreply.github.com>
2018-10-07 22:43:29 +0530

    Accept

--- app/controllers/api/v1/AcceptSuiteController.rb
+++ app/controllers/api/v1/AcceptSuiteController.rb
@@ -1,6 +1,5 @@
 require 'authorizenet' 
-# require_relative '../../../../../sample-code-ruby-master/sample-code-ruby-master/PaymentTransactions/create-an-accept-payment-transaction.rb'
-require 'authorizenetSample'
+require 'authorizenetsample'
 
 
 def Test
