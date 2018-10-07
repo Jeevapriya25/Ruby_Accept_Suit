2c5997ea7d22ad66b7734afbf8882c9498e5df85
JeevapriyaC <30900716+JeevapriyaC@users.noreply.github.com>
2018-10-07 23:09:42 +0530

    test

--- app/controllers/api/v1/AcceptSuiteController.rb
+++ app/controllers/api/v1/AcceptSuiteController.rb
@@ -1,10 +1,18 @@
-require 'authorizenet' 
+# require 'authorizenet' 
 require 'authorizenetsample'
 
-
-def Test
-	response = create_an_accept_payment_transaction()
-	puts response
+module Api
+	module V1
+		class acceptjsController < ApplicationController
+			def Test
+				response = create_an_accept_payment_transaction()
+				# puts response
+				render json:{success: 'SUCCESS',message: 'Created transaction',data:response},status: :ok
+			end
+		end
+	end
 end
 
-Test()
+
+
+# Test()
