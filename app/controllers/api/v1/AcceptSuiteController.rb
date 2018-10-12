55b2ca347a218475b99c6e3f048f3b59e6e564cc
JeevapriyaC <30900716+JeevapriyaC@users.noreply.github.com>
2018-10-12 22:23:13 +0530

    accept suite controller

--- app/controllers/api/v1/AcceptSuiteController.rb
+++ app/controllers/api/v1/AcceptSuiteController.rb
@@ -5,7 +5,7 @@ module Api
 	module V1
 		class AcceptjsController < ApplicationController
 			def Test
-				# response = create_an_accept_payment_transaction()
+			 response = create_an_accept_payment_transaction()
 				# puts response
 				render json:{success: 'SUCCESS',message: 'Created transaction',data:},status: :ok
 			end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,11 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  namespace 'api' do
-    namespace 'v1' do
-      resources :acceptjs
-    end
-  end
-end
+#   namespace :api do
+#     namespace :v1 do
+#       resources :Accetsuites
+#     end
+#   end
+ # root controller: :accetsuites
+ # resources :accetsuites
+ get "acceptsuite/index" => "acceptsuites#index"
+ end
